a5e500a06851fd7dbd1c452a49fa493370165446
lazalajochy <lazala1401jochy@outlook.com>
2022-06-18 10:12:34 -0400

    counting valley

---
+++ countingValley/valley.js
@@ -0,0 +1,17 @@
+var path = "UDUDDUUUDUDUDUUDUUDDDDDUDUDDDDUUDDUDDUUUUDUUDUDDDDUDUDUUUDDDUUUDUDDUUDDDUUDDUDDDUDUUDUUDUUDUDDDUUUUU"; //waiting for 6
+var result = countValley(path);
+function countValley(path) {
+  
+  var result = 0, d = 0; 
+  for(var i = 0; i <= path.length -1; i++){
+    if(d <  0){
+      path[i] == "D" ? d-- : d++;
+
+      d == 0 ? result++ : result = result;
+    }else{
+      path[i] == "D" ? d-- : d++;
+
+    }
+  }
+  document.write(result);
+}
