c7b0097d14fc72bdb7bfa7f8a255a73b15e64d87
lazalajochy <lazala1401jochy@outlook.com>
2022-06-18 10:21:48 -0400

    counting valley

--- countingValley/valley.js
+++ countingValley/valley.js
@@ -6,7 +6,6 @@ function countValley(path) {
   for(var i = 0; i <= path.length -1; i++){
     if(d <  0){
       path[i] == "D" ? d-- : d++;
-
       d == 0 ? result++ : result = result;
     }else{
       path[i] == "D" ? d-- : d++;
