4391cfe1cef9d57c729217bc6b0aae656bb8bd5c
lazalajochy <lazala1401jochy@outlook.com>
2022-06-23 09:56:55 -0400

    square

---
+++ square/square.js
@@ -0,0 +1,24 @@
+//https://www.hackerrank.com/challenges/sherlock-and-squares/problem?isFullScreen=true
+
+var a = 24,
+  b = 49;
+var result = square(a, b);
+function squares(a, b) {
+  var result = 0,
+    impar = 0,
+    x = 1;
+  for (var i = 0; i < b; i++) {
+    if (impar == 0) {
+      impar++;
+    } else {
+      impar += 2;
+      x += impar;
+    }
+    if (x >= a && x <= b) {
+      result++;
+    }
+    if (x > b) break;
+  }
+
+  return result;
+}
