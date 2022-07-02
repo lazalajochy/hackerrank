96c066dd24fa31604a5f158832965095559cf91f
lazalajochy <lazala1401jochy@outlook.com>
2022-07-01 23:24:11 -0400

    extra long factorial

---
+++ extraLongFactorials/extraLongFactorials.js
@@ -0,0 +1,14 @@
+var n = 30;
+var result = extraLongFactorials(n);
+
+function extraLongFactorials(n) {
+  var result = 1n,
+    n = BigInt(n);
+
+  while (n > 0) {
+    result *= n;
+    n--;
+  }
+  console.log(result.toString());
+  
+}
