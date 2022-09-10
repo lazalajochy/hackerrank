c4cf467a99347ba31a71c4916b34a3f8ddcd2108
lazalajochy <lazala1401jochy@outlook.com>
2022-09-10 16:18:05 -0400

    mars Exploration

---
+++ marsExploration/marsExploration.js
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/mars-exploration/problem?isFullScreen=true
+
+var s = 'SOSOOSOSOSOSOSSOSOSOSOSOSOS';
+var result = marsExploration(s);
+
+function marsExploration(s) {
+    debugger
+    var result = 0, arr = [], letter = '', msg = "SOS";
+    for (var i = 0; i < s.length; i++) {
+        arr.push(s[i]);
+        if (arr.length == 3) {
+            for (var j = 0; j < arr.length; j++) {
+                if (arr[j] !== msg[j]) result++;
+            }
+            arr = [];
+        }
+    }
+    return result;
+}
