c5ed405de9ad058261e29f12cf9be171ac22dc3d
lazalajochy <lazala1401jochy@outlook.com>
2022-11-25 22:56:32 -0400

    cut the sticks

--- cutTheSticks/cutTheSticks.js
+++ cutTheSticks/cutTheSticks.js
@@ -1,19 +1,22 @@
 //https://www.hackerrank.com/challenges/cut-the-sticks/problem?isFullScreen=true
 
 // var arr = [1, 2, 3];
- var arr = [5, 4, 4, 2, 2, 8] //waiting  for 6,4,2,1
- varresult = cutTheSticks(arr);
+var arr = [5, 4, 4, 2, 2, 8]; //waiting  for 6,4,2,1
+varresult = cutTheSticks(arr);
 
- function cutTheSticks(arr) {
-     var newArr = [];
-     while (arr.length !== 0) {
-         var aux = Math.min(...arr)
-         document.write(arr.length)
+function cutTheSticks(arr) {
+  var newArr = [],
+    result = [];
+  while (arr.length !== 0) {
+    var aux = Math.min(...arr);
+    result.push(arr.length);
 
-         for (var j = 0; j < arr.length; j++) {
-             if (arr[j] - aux !== 0) newArr.push(arr[j] - aux);
-         };
-         arr = newArr;
-         newArr = [];
-     };
- };
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[j] - aux !== 0) newArr.push(arr[j] - aux);
+    }
+    arr = newArr;
+    newArr = [];
+  }
+
+  return result;
+}
