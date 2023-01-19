4306c43e3ba0dcd902b68c37970235113a09fffe
lazalajochy <lazala1401jochy@outlook.com>
2023-01-18 21:11:12 -0400

    mark&Toys

--- mark&Toys/mark&Toys.js
+++ mark&Toys/mark&Toys.js
@@ -4,7 +4,7 @@ var prices = [1, 12, 5, 111, 200, 1000, 10], k = 50;
 function maximumToys(prices, k) {
     prices.sort((a, b) => a - b)
     let sum = 0, previousValue = 0, count = 0;
-    prices.reduce((acc, item) => {
+    prices.sort((acc, item) => {
         sum = sum + item;
         if (sum <= k) {
             count++;
